760d091a7c77804134cbe1b13da03daa419b3147
15tkdgns <15tkdgbs@gmail.com>
2026-02-05 17:33:30 +0900

    fix: add missing React import for Vercel build

--- src/components/FileUploader.jsx
+++ src/components/FileUploader.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './FileUploader.css'
 
 function FileUploader({ label, accept, file, onFileSelect, onFileRemove }) {
